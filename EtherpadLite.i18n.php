f6daa1e4573970ea6a69c1d6d27d05649cbdf872
Thomas Gries <wikinaut@users.mediawiki.org>
2012-02-13 20:22:32 +0000

    follow-up 111327 . here we got: using wfBoolToStr(), wfAppendUrl, Http::isValidURI, and the real beta etherpad server url as agreed by the eplite team

--- EtherpadLite.i18n.php
+++ EtherpadLite.i18n.php
@@ -13,4 +13,5 @@ $messages = array();
  */
 $messages['en'] = array(
 	'etherpadlite-desc' => 'Provides a method to embed one or many Etherpad Lite pads (which are hosted on local or external Etherpad Lite server/s) on MediaWiki pages. It adds an &lt;eplite&gt; parser tag.',
+	'etherpadlite-invalid-pad-url' => '\'$1\' is not a valid Etherpad Lite URL or pad name.',
 );
--- EtherpadLite.php
+++ EtherpadLite.php
@@ -7,7 +7,7 @@
  *
  * The extension adds a tag "eplite" to the MediaWiki parser and
  * provides a method to embed Etherpad Lite pads on MediaWiki pages.
- * The Etherpad Lite server is not part of the extension.
+ * An Etherpad Lite server is not part of the extension.
  *
  * Usage:
  *
@@ -21,14 +21,17 @@
  * Add the following lines in LocalSettings.php:
  *
  * require_once( "$IP/extensions/EtherpadLite/EtherpadLite.php" );
- * $wgEtherpadLiteDefaultPadUrl    = "http://www.your-pad-server.org/p/";
+ * Etherpad Lite host server Url. 
+ * The shown one is a test server: it is not meant for production.
+ * $wgEtherpadLiteDefaultPadUrl    = "http://beta.etherpad.org/p/";
  * $wgEtherpadLiteDefaultWidth     = "600px";
  * $wgEtherpadLiteDefaultHeigth    = "400px";
  *
  * Prerequisite:
  *
- * Etherpad Lite host server (example)
- * $wgEtherpadLiteDefaultPadUrl = "http://www.example.com/p/";
+ * You need at least one Etherpad Lite host server
+ * The shown one is a test server: it is not meant for production.
+ * $wgEtherpadLiteDefaultPadUrl = "http://beta.etherpad.org/p/";
  *
  * For setting up your own Etherpad Lite server (based on node.js) see 
  * Etherpad Lite homepage https://github.com/Pita/etherpad-lite
@@ -61,7 +64,7 @@ $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'EtherpadLite',
 	'author' => array( 'Thomas Gries' ),
-	'version' => '1.04 20120212',
+	'version' => '1.05 20120213',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:EtherpadLite',
 	'descriptionmsg' => 'etherpadlite-desc',
 );
@@ -79,7 +82,7 @@ function wfEtherpadLiteParserInit( $parser ) {
 
 # Define a default Etherpad Lite server Url and base path
 # this server is used unless a distinct server is defined by id="..."
-$wgEtherpadLiteDefaultPadUrl    = "http://www.example.com/p/";
+$wgEtherpadLiteDefaultPadUrl    = "http://beta.etherpad.org/p/";
 $wgEtherpadLiteDefaultWidth     = "300px";
 $wgEtherpadLiteDefaultHeight    = "200px";
 $wgEtherpadLiteMonospacedFont   = false;
@@ -88,17 +91,14 @@ $wgEtherpadLiteShowLineNumbers  = true;
 $wgEtherpadLiteShowChat         = true;
 $wgEtherpadLiteShowAuthorColors = true;
 
-
-function wfEtherpadLiteStringFromBoolean( $bool ) {
-	return ( $bool ) ? "true" : "false";
-}
-
 function wfEtherpadLiteRender( $input, $args, $parser, $frame ) {
 
 	global $wgUser;
 	global $wgEtherpadLiteDefaultPadUrl, $wgEtherpadLiteDefaultWidth, $wgEtherpadLiteDefaultHeight,
 		$wgEtherpadLiteMonospacedFont, $wgEtherpadLiteShowControls, $wgEtherpadLiteShowLineNumbers,
 		$wgEtherpadLiteShowChat, $wgEtherpadLiteShowAuthorColors;
+
+	# check the user input
 
 	# undefined id= attributes are replaced by id="" and result
 	# in Etherpad Lite server showing its entry page - where you can open a new pad.
@@ -107,54 +107,73 @@ function wfEtherpadLiteRender( $input, $args, $parser, $frame ) {
 	$args['height']   = ( isset( $args['height'] ) ) ? $args['height'] : $wgEtherpadLiteDefaultHeight;
 	$args['width']    = ( isset( $args['width'] ) ) ? $args['width'] : $wgEtherpadLiteDefaultWidth;
 	
-	$useMonospaceFont = wfEtherpadLiteStringFromBoolean(
+	$useMonospaceFont = wfBoolToStr(
 		( ( isset( $args['monospaced-font'] ) ) ? filter_var( $args['monospaced-font'], FILTER_VALIDATE_BOOLEAN ) : $wgEtherpadLiteMonospacedFont )
 	);
 
-	$showControls = wfEtherpadLiteStringFromBoolean(
+	$showControls = wfBoolToStr(
 		( ( isset( $args['show-controls'] ) ) ? filter_var( $args['show-controls'], FILTER_VALIDATE_BOOLEAN ) : $wgEtherpadLiteShowControls )
 	);
 
-	$showLineNumbers = wfEtherpadLiteStringFromBoolean(
+	$showLineNumbers = wfBoolToStr(
 		( ( isset( $args['show-linenumbers'] ) ) ? filter_var( $args['show-linenumbers'], FILTER_VALIDATE_BOOLEAN ) : $wgEtherpadLiteShowLineNumbers )
 	);
 			
-	$showChat = wfEtherpadLiteStringFromBoolean(
+	$showChat = wfBoolToStr(
 		( ( isset( $args['show-chat'] ) ) ? filter_var( $args['show-chat'], FILTER_VALIDATE_BOOLEAN ) : $wgEtherpadLiteShowChat )
 	);
 	
-	$noColors = wfEtherpadLiteStringFromBoolean(
+	$noColors = wfBoolToStr(
 		! ( ( isset( $args['show-colors'] ) ) ? filter_var( $args['show-colors'], FILTER_VALIDATE_BOOLEAN ) : $wgEtherpadLiteShowAuthorColors )
 	);
 
-	$args['src'] = Sanitizer::cleanUrl ( 
-		( isset( $args['src'] ) ) ? $args['src'] : $wgEtherpadLiteDefaultPadUrl
-	);
-
-	# preset the pad username from MediaWiki username or IP
+	# src= is the pad server base url and is user input in <eplite src= > tag from MediaWiki page
+	# id=  is the pad name (also known as pad id) and is user input in <eplite id=  > tag from MediaWiki page
 	
+	$src = ( isset( $args['src'] ) ) ? $args['src'] : $wgEtherpadLiteDefaultPadUrl;
+	
+	if ( !Http::isValidURI( $src ) ) {
+		return wfMsg( 'etherpadlite-invalid-pad-url', htmlspecialchars( $src ) );
+	} else {
+		$args['src'] = Sanitizer::cleanUrl ( $src );
+	}
+	
+	# let's use the MediaWiki santizer for our user attributes
+	
+	$sanitizedAttributes = Sanitizer::validateAttributes( $args, array ( "width", "height", "id", "src" ) );
+
+	$url = Sanitizer::cleanUrl( preg_replace( "/\/+$/", "", $sanitizedAttributes['src'] ) . "/" . $sanitizedAttributes['id'] );
+
+	# just check again with the pad id appended
+
+	if ( !Http::isValidURI( $url ) ) {
+		return wfMsg( 'etherpadlite-invalid-pad-url', htmlspecialchars( $url ) );
+	}
+	
+	# preset the pad username from MediaWiki username or IP
+	# this not strict, as the pad username can be overwritten in the pad
+	#
 	# attention: 
 	# 1. we must render the page for each visiting user to get their username
 	# 2. the pad username can currently be overwritten when editing the pad
 
 	$parser->disableCache();
-	$userName  = rawurlencode( $wgUser->getName() );
-	
-	$sanitizedAttributes = Sanitizer::validateAttributes( $args, array ( "width", "height", "id", "src" ) );
+
+	$url = wfAppendQuery( $url, array(
+			"showControls"     => $showControls,
+			"showChat"         => $showChat,
+			"showLineNumbers"  => $showLineNumbers,
+			"useMonospaceFont" => $useMonospaceFont,
+			"noColors"         => $noColors,
+			"userName"         => rawurlencode( $wgUser->getName() ),
+		)
+	);
 	
 	$iframeAttributes = array(
  		"style" => "width:" . $sanitizedAttributes['width'] . ";" .
-				"height:" . $sanitizedAttributes['height'],
+			"height:" . $sanitizedAttributes['height'],
 		"class" => "eplite-iframe-" . $sanitizedAttributes['id'] ,
-		"src"   => Sanitizer::cleanUrl( 
-			$sanitizedAttributes['src'] . "/" . $sanitizedAttributes['id'] . 
-			"?showControls=$showControls" .
-			"&showChat=$showChat" .
-			"&showLineNumbers=$showLineNumbers" .
-			"&useMonospaceFont=$useMonospaceFont" .
-			"&noColors=$noColors" .
-			"&userName=$userName"
-		),
+		"src"   => Sanitizer::cleanUrl( $url ),
 	);
 
 	$output = Html::rawElement(
